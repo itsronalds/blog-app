3766ab144e438bb9d6eb046ea38a42da7c482cdd
Ronald Abu Saleh <lancerrnld@gmail.com>
2022-03-10 23:14:13 -0400

    Db file refactor

--- server/src/config/db/index.js
+++ server/src/config/db/index.js
@@ -5,10 +5,10 @@ import { promisify } from 'util';
 dotenv.config();
 
 const DB_CONFIG = {
-  host: 'localhost',
-  user: 'root',
-  password: '01071998',
-  database: 'blog',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   port: process.env.DB_PORT,
   charset: 'utf8mb4',
 };
